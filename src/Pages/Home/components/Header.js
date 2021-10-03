5feb60f013f300b18f50e5b4631ff86e2576e882
Gabriel Basaglia <gabagtc@outlook.com>
2021-10-03 08:32:18 -0300

    Initial Code

---
+++ src/Pages/Home/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header (){
+    return (
+        <header className="header">
+            <div className="toolbar">
+                <span>Conecta Dev</span>
+                <div>
+                    <button>Novo Post</button>
+                    <span>img1</span>
+                    <span>img2</span>
+                </div>
+            </div>
+        </header>
+    )
+}
+export default Header;
